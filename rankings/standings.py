473192308f27a06b6ab8814df47d1c70c6dac426
Carl Aslund <caslund@g.hmc.edu>
2018-01-12 23:50:43 -0500

    Add match data as .csv files

---
+++ rankings/standings.py
@@ -0,0 +1,3 @@
+'''
+This file will read in match data from csv files (obtained from football-data.co.uk) and proceed to rank teams, either on a match day or on a particular date, through the season.
+'''
